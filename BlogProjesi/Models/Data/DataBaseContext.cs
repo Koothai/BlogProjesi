e0c194f9830578ef49bdff9ff25611e5d74c7376
Koothai <osmankutay123@hotmail.com>
2023-01-09 11:21:22 +0300

    Add project files.

---
+++ BlogProjesi/Models/Data/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using BlogProjesi.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProjesi.Models.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(new User(1, "Kutay", "1234"));
+            modelBuilder.Entity<Article>().Property(x => x.CreatedTime).HasDefaultValueSql("getutcdate()");
+        }
+
+    }
+}
